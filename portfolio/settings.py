4881b6bc43b00cc617981ccb7c18d82645d66e2c
dimitri-hoareau <dimitrihoareau2@gmail.com>
2024-02-21 20:28:58 +0400

    add nginx to serve staticfilesé

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,14 +15,12 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # Load environment variables from .env.dev file
 dotenv_path = os.path.join(BASE_DIR, '.env.dev')
-print(dotenv_path)
 load_dotenv(dotenv_path)
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split()
@@ -126,8 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
