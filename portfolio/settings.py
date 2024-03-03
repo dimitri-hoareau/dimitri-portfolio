11612f5558100dd4bfedccb152268c7eafd9b9a5
dimitri-hoareau <dimitrihoareau2@gmail.com>
2024-03-03 16:54:11 +0400

    Fix environment variables syntax in GitHub Actions workflow

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -133,4 +133,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
