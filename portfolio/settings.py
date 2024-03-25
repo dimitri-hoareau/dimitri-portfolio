a9dbfd7a4e90b1f8831b9dd59095c63c48801914
dimitri-hoareau <dimitrihoareau2@gmail.com>
2024-03-25 21:17:58 +0400

    change CORS policy for development

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -143,8 +142,9 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 #Only for development
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://dimitrihoareau.fr",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://yourfrontenddomain.com",
+#     # other allowed domains...
+# ]
