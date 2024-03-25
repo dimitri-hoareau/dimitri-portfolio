ee98c2df909e3f082d339bd41eaa25aad9ca1fba
dimitri-hoareau <dimitrihoareau2@gmail.com>
2024-03-25 12:17:20 +0400

    updat serializer to get tools's name with endpoint

--- projectapi/serializers.py
+++ projectapi/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework.serializers import ModelSerializer
  
-from mainpage.models import Project
+from mainpage.models import Project, Tool
  
+
+
+class ToolSerializer(ModelSerializer):
+    class Meta:
+        model = Tool
+        fields = ['id', 'name']
+
+
 class ProjectSerializer(ModelSerializer):
+    
+    tools = ToolSerializer(many=True, read_only=True)
  
     class Meta:
         model = Project
