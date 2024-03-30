90b1e1c14fbcabdd597509f19e86af3a671c8232
dimitri-hoareau <dimitrihoareau2@gmail.com>
2024-03-30 12:26:43 +0100

    add animations

--- mainpage/static/js/navbar-menu.js
+++ mainpage/static/js/navbar-menu.js
@@ -8,7 +8,6 @@ window.addEventListener('scroll', () => {
     const aboutSectionPos = aboutSection.getBoundingClientRect();
     const projectsSectionPos = projectsSection.getBoundingClientRect();
 
-    console.log(aboutSectionPos)
 
     if ((aboutSectionPos.top <= 0 && aboutSectionPos.bottom >= 47) || (projectsSectionPos.top <= 0 && projectsSectionPos.bottom >= 47)) {
         // Si l'utilisateur se trouve dans la section "about" ou "projects"
@@ -32,4 +31,36 @@ document.querySelector('.burger').addEventListener('click', () => {
     document.querySelector('.burger .line1').classList.toggle('toggle');
     document.querySelector('.burger .line2').classList.toggle('toggle');
     document.querySelector('.burger .line3').classList.toggle('toggle');
+});
+
+
+
+const aboutText = document.querySelectorAll('.about-text');
+const aboutImage = document.querySelectorAll('.about-image');
+const skillsText = document.querySelectorAll('.skills-list-item');
+const footerLeft = document.querySelectorAll('.footer-container-left');
+const footerMap = document.querySelectorAll('.footer-map');
+
+
+ScrollReveal().reveal('.about-section', { 
+    origin: 'right',
+    beforeReveal: function(domEl) {
+        aboutText.forEach(text => text.classList.add('right-transition'));
+        aboutImage[0].classList.add('left-transition');
+    }
+});
+
+ScrollReveal().reveal('.skills-section', { 
+    origin: 'right',
+    beforeReveal: function(domEl) {
+        skillsText.forEach(text => text.classList.add('appears'));
+    }
+});
+
+ScrollReveal().reveal('.contact-section', { 
+    origin: 'right',
+    beforeReveal: function(domEl) {
+        footerLeft[0].classList.add('left-transition');
+        footerMap[0].classList.add('right-transition');
+    }
 });
